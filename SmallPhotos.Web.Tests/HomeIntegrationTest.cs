8f199146be191c5c6ba860c0b37dcc251538b1d7
Andy Whitfield <andy@andywhitfield.me.uk>
2021-03-29 19:25:00 +0100

    Added skeleton test

---
+++ SmallPhotos.Web.Tests/HomeIntegrationTest.cs
@@ -0,0 +1,40 @@
+using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using SmallPhotos.Data;
+using SmallPhotos.Model;
+using Xunit;
+
+namespace SmallPhotos.Web.Tests
+{
+    public class HomeIntegrationTest : IAsyncLifetime
+    {
+        private readonly IntegrationTestWebApplicationFactory _factory = new IntegrationTestWebApplicationFactory();
+
+        public async Task InitializeAsync()
+        {
+            using var serviceScope = _factory.Services.CreateScope();
+            using var context = serviceScope.ServiceProvider.GetRequiredService<SqliteDataContext>();
+            context.Migrate();
+            var userAccount = await context.UserAccounts.AddAsync(new UserAccount { AuthenticationUri = "http://test/user/1" });
+            await context.SaveChangesAsync();
+        }
+
+        [Fact]
+        public async Task Should_display_expected_items()
+        {
+            using var client = _factory.CreateAuthenticatedClient();
+            var response = await client.GetAsync("/");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            responseContent.Should().Contain("Logout");
+        }
+
+        public Task DisposeAsync()
+        {
+            _factory.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SmallPhotos.Web.Tests/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,53 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using SmallPhotos.Data;
+
+namespace SmallPhotos.Web.Tests
+{
+    public class IntegrationTestWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        private readonly SqliteConnection _connection;
+        private readonly DbContextOptions<SqliteDataContext> _options;
+
+        public IntegrationTestWebApplicationFactory()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+            _options = new DbContextOptionsBuilder<SqliteDataContext>().UseSqlite(_connection).Options;
+        }
+
+        protected override IHostBuilder CreateHostBuilder() => Host
+            .CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(x => x.UseStartup<Startup>().UseTestServer().ConfigureTestServices(services =>
+            {
+                services.Replace(ServiceDescriptor.Scoped<SqliteDataContext>(_ => new SqliteDataContext(_options)));
+                services
+                    .AddAuthentication("Test")
+                    .AddScheme<AuthenticationSchemeOptions, TestStubAuthHandler>("Test", null);
+            }));
+
+        public HttpClient CreateAuthenticatedClient(bool allowAutoRedirect = true)
+        {
+            var client = CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = allowAutoRedirect });
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Test");
+            return client;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (disposing)
+                _connection.Dispose();
+        }
+    }
+}
