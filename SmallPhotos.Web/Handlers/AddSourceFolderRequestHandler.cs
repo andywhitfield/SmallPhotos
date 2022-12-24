15d51c4b49685516fee7ca3824b0a44557fa8388
Andy Whitfield <andy@andywhitfield.me.uk>
2022-12-24 16:04:28 +0000

    Added basic validation check to avoid duplicate folder sources
    Just returns a 400 response, which isn't the best. A TODO added to improve this.

--- SmallPhotos.Web/Handlers/AddSourceFolderRequestHandler.cs
+++ SmallPhotos.Web/Handlers/AddSourceFolderRequestHandler.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -6,38 +8,47 @@ using Microsoft.Extensions.Logging;
 using SmallPhotos.Data;
 using SmallPhotos.Web.Handlers.Models;
 
-namespace SmallPhotos.Web.Handlers
+namespace SmallPhotos.Web.Handlers;
+
+public class AddSourceFolderRequestHandler : IRequestHandler<AddSourceFolderRequest, bool>
 {
-    public class AddSourceFolderRequestHandler : IRequestHandler<AddSourceFolderRequest, bool>
+    private readonly ILogger<AddSourceFolderRequestHandler> _logger;
+    private readonly IUserAccountRepository _userAccountRepository;
+    private readonly IAlbumRepository _albumRepository;
+
+    public AddSourceFolderRequestHandler(
+        ILogger<AddSourceFolderRequestHandler> logger,
+        IUserAccountRepository userAccountRepository,
+        IAlbumRepository albumRepository)
     {
-        private readonly ILogger<AddSourceFolderRequestHandler> _logger;
-        private readonly IUserAccountRepository _userAccountRepository;
-        private readonly IAlbumRepository _albumRepository;
+        _logger = logger;
+        _userAccountRepository = userAccountRepository;
+        _albumRepository = albumRepository;
+    }
 
-        public AddSourceFolderRequestHandler(
-            ILogger<AddSourceFolderRequestHandler> logger,
-            IUserAccountRepository userAccountRepository,
-            IAlbumRepository albumRepository)
+    public async Task<bool> Handle(AddSourceFolderRequest request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(request.Folder))
+            return false;
+        
+        if (!Path.IsPathRooted(request.Folder))
         {
-            _logger = logger;
-            _userAccountRepository = userAccountRepository;
-            _albumRepository = albumRepository;
+            _logger.LogWarning($"Cannot add a folder which isn't a fully-qualified path [{request.Folder}]");
+            return false;
         }
 
-        public async Task<bool> Handle(AddSourceFolderRequest request, CancellationToken cancellationToken)
-        {
-            if (string.IsNullOrEmpty(request.Folder))
-                return false;
-            
-            if (!Path.IsPathRooted(request.Folder))
-            {
-                _logger.LogWarning($"Cannot add a folder which isn't a fully-qualified path [{request.Folder}]");
-                return false;
-            }
+        var user = await _userAccountRepository.GetUserAccountAsync(request.User);
+        var currentSources = await _albumRepository.GetAllAsync(user);
 
-            var user = await _userAccountRepository.GetUserAccountAsync(request.User);
-            await _albumRepository.AddAsync(user, request.Folder, request.Recursive);
-            return true;
+        var directoryToAdd = Path.GetFullPath(request.Folder);
+        if (currentSources.Any(s => string.Equals(Path.GetFullPath(s.Folder ?? ""), directoryToAdd, StringComparison.OrdinalIgnoreCase)))
+        {
+            // TODO: this just ends up returning a 400 to the client - should actually show a reason on the UI
+            _logger.LogWarning($"The requested folder to add [{request.Folder}] already exists, can't add a duplicate");
+            return false;
         }
+
+        await _albumRepository.AddAsync(user, request.Folder, request.Recursive);
+        return true;
     }
 }
